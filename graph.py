2bd5c5c844caabc1f91a5d111dbaa3c4ae41df7b
Jonas Werner <werner-jonas@outlook.com>
2025-01-23 21:27:20 +0100

    docu

--- graph.py
+++ graph.py
@@ -9,13 +9,15 @@ class Graph(nx.Graph):
         self, num_intersections=10, num_borders=3, weight_range=(1, 10), file_path=None
     ):
         """
-        Initialize a Graph object with a specified number of intersections and borders.
+        A class to represent a graph.
+
+        The class inherits from the networkx.Graph class and adds methods to save the graph to a file and generate a plot.
 
         Parameters:
-        num_intersections (int): The number of intersection nodes to create.
-        num_borders (int): The number of border nodes to create.
-        weight_range (tuple): A tuple specifying the range of weights for the edges.
-        file_path (str): Path to a file to load the graph from. If provided, other parameters are ignored.
+            - num_intersections (int): The number of intersection nodes to create.
+            - num_borders (int): The number of border nodes to create.
+            - weight_range (tuple): A tuple specifying the range of weights for the edges.
+            - file_path (str): Path to a file to load the graph from. If provided, other parameters are ignored.
 
         The method creates nodes labeled as 'intersection' and 'border', and adds edges between them with random weights.
         Self-loops are removed from the graph.
@@ -64,6 +66,9 @@ class Graph(nx.Graph):
     def generate_plot(self):
         """
         Visualize the graph.
+
+        Returns:
+        plt: A matplotlib.pyplot object.
         """
 
         pos = nx.spring_layout(self, seed=42)
