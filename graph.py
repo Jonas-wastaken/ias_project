f8a183c6ff028473316b1a64e9e9643920cd79f7
Jonas Werner <werner-jonas@outlook.com>
2025-01-27 11:29:30 +0100

    docu

--- graph.py
+++ graph.py
@@ -6,25 +6,26 @@ import matplotlib.pyplot as plt
 
 class Graph(nx.Graph):
     def __init__(
-        self, num_intersections=10, num_borders=3, weight_range=(1, 10), file_path=None
+        self,
+        num_intersections: int = 10,
+        num_borders: int = 3,
+        weight_range: tuple[int, int] = (1, 10),
     ):
-        """
-        A class to represent a graph.
+        """A class to represent a graph.
 
         The class inherits from the networkx.Graph class and adds methods to save the graph to a file and generate a plot.
-
-        Parameters:
-            - num_intersections (int): The number of intersection nodes to create.
-            - num_borders (int): The number of border nodes to create.
-            - weight_range (tuple): A tuple specifying the range of weights for the edges.
-            - file_path (str): Path to a file to load the graph from. If provided, other parameters are ignored.
-
-        The method creates nodes labeled as 'intersection' and 'border', and adds edges between them with random weights.
+        Creates nodes labeled as 'intersection' and 'border', and adds edges between them with random weights.
         Self-loops are removed from the graph.
+
+        Args:
+            num_intersections (int, optional): The number of intersection nodes to create. Defaults to 10.
+            num_borders (int, optional): The number of border nodes to create. Defaults to 3.
+            weight_range (tuple, optional): A tuple specifying the range of weights for the edges. Defaults to (1, 10).
         """
 
         super().__init__()
 
+        # Add nodes of type intersection
         super().add_nodes_from(
             [
                 (f"intersection_{i}", {"type": "intersection"})
@@ -32,10 +33,12 @@ class Graph(nx.Graph):
             ]
         )
 
+        # Add nodes of type border
         super().add_nodes_from(
             [(f"border_{i}", {"type": "border"}) for i in range(num_borders)]
         )
 
+        # Add edges between intersections
         for i in range(num_intersections):
             for j in range(random.randint(2, 4)):
                 super().add_edge(
@@ -44,6 +47,7 @@ class Graph(nx.Graph):
                     weight=random.randint(weight_range[0], weight_range[1]),
                 )
 
+        # Add edges between intersections and borders
         for i in range(num_borders):
             super().add_edge(
                 f"border_{i}",
@@ -53,27 +57,24 @@ class Graph(nx.Graph):
 
         super().remove_edges_from(nx.selfloop_edges(self))
 
-    def save(self, filename):
-        """
-        Save the graph to a file.
+    def save(self, filename: str):
+        """Save class instance to a pickle file.
 
-        Parameters:
-        filename (str): The name of the file to save the graph to.
+        Args:
+            filename (str): The name of the file to save the class instance to.
         """
 
         pickle.dump(self, open(filename, "wb"))
 
-    def generate_plot(self):
-        """
-        Visualize the graph.
+    def generate_plot(self) -> plt:
+        """Generate a plot of the graph.
 
         Returns:
-        plt: A matplotlib.pyplot object.
+            plt: A matplotlib.pyplot object with the graph plotted.
         """
 
         pos = nx.spring_layout(self, seed=42)
 
-        # Change color of nodes based on type
         node_color = [
             "red" if self.nodes[n]["type"] == "border" else "blue" for n in self.nodes
         ]
