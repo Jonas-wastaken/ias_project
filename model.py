53db035a20ce260e724099254164300e89a97f0f
Jonas Werner <werner-jonas@outlook.com>
2025-02-08 00:07:48 +0100

    add config options

--- model.py
+++ model.py
@@ -20,16 +20,22 @@ class TrafficModel(mesa.Model):
             Advances the environment to next state.
     """
 
-    def __init__(self, num_agents: int, seed: int = None):
+    def __init__(self, num_agents: int, seed: int = None, **kwargs):
         """Initializes a new traffic environment.
 
         Args:
             num_agents (int): Number of agents to spawn.
             seed (int, optional): Seed used in model generation. Defaults to None.
+            **kwargs: Additional keyword arguments for configuring the graph object.
         """
         super().__init__(seed=seed)
+        self.kwargs = kwargs
         self.num_agents = num_agents
-        self.grid = Graph()
+        self.grid = Graph(
+            num_intersections=self.kwargs.get("num_intersections", 10),
+            num_borders=self.kwargs.get("num_borders", 3),
+            weight_range=self.kwargs.get("weight_range", (1, 10)),
+        )
         _agents = CarAgent.create_agents(model=self, n=num_agents)
         self.agent_paths = {agent.unique_id: agent.path.copy() for agent in _agents}
 
