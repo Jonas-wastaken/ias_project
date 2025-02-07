ad221ee932ebe24a7fd85bc19591593614bb89e9
Jonas Werner <werner-jonas@outlook.com>
2025-02-07 15:31:18 +0100

    restructure code

--- graph_viz.py
+++ graph_viz.py
@@ -1,21 +1,69 @@
 import networkx as nx
 import plotly.graph_objects as go
+from model import TrafficModel
 
 
 class TrafficGraph(go.Figure):
-    def __init__(self, model):
+    """A plotly figure of the traffic grid.
+
+    Places nodes and edges of a traffic grid on a plotly figure. Nodes are colored according to their type. Figure displays information about the nodes on hover.
+
+    Inherits from go.Figure.
+
+    Attributes:
+        model (TrafficModel): The traffic model to visualize.
+
+    ## Methods
+        **get_coords_edges(self) -> tuple**:
+            Get the x and y coordinates of the edges.
+        **get_coords_nodes(self) -> tuple**:
+            Get the x and y coordinates of the nodes.
+        **create_trace_edges(self, edge_x: list, edge_y: list) -> go.Scatter**:
+            Create a plotly trace for the edges.
+        **create_trace_nodes(self, node_x: list, node_y: list, node_color: list) -> go.Scatter**:
+            Create a plotly trace for the nodes.
+        **create_node_color(self) -> list**:
+            Create a list of colors for the nodes.
+        **create_node_text(self) -> list**:
+            Create a list of text for the nodes.
+    """
+
+    def __init__(self, model: TrafficModel):
+        """Create a plotly figure of the traffic grid.
+
+        Args:
+            model (TrafficModel): The traffic model to visualize.
+        """
         super().__init__()
-        self._model = model  # Use a private attribute to store the model
-        self.create_graph()
+        self._model = model
+        pos = nx.spring_layout(self._model.grid, seed=42)  # Create layout for the graph
+        nx.set_node_attributes(
+            self._model.grid, pos, "pos"
+        )  # Assign positions to nodes
+        edge_x, edge_y = self.get_coords_edges()
+        edge_trace = self.create_trace_edges(edge_x, edge_y)
+        node_x, node_y = self.get_coords_nodes()
+        node_color = self.create_node_color()
+        node_trace = self.create_trace_nodes(node_x, node_y, node_color)
+        node_trace.text = self.create_node_text()
 
-    def create_graph(self):
-        # Create a layout using the spring layout algorithm
-        pos = nx.spring_layout(self._model.grid, seed=42)
+        # Create the figure
+        self.add_traces([edge_trace, node_trace])
+        self.update_layout(
+            title=dict(text="<br>Traffic Grid", font=dict(size=16)),
+            showlegend=False,
+            hovermode="closest",
+            margin=dict(b=20, l=5, r=5, t=40),
+            xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+            yaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+        )
 
-        # Assign the positions to the nodes using dictionary comprehension
-        nx.set_node_attributes(self._model.grid, pos, "pos")
+    def get_coords_edges(self) -> tuple:
+        """Get the x and y coordinates of the edges.
 
-        # Create a list to store the x and y coordinates of the edges
+        Returns:
+            tuple: Lists of x and y coordinates of the edges
+        """
         edge_x = []
         edge_y = []
         for edge in self._model.grid.edges():
@@ -28,16 +76,14 @@ class TrafficGraph(go.Figure):
             edge_y.append(y1)
             edge_y.append(None)
 
-        # Create a trace for the edges
-        edge_trace = go.Scatter(
-            x=edge_x,
-            y=edge_y,
-            line=dict(width=0.5, color="#888"),
-            hoverinfo="none",
-            mode="lines",
-        )
+        return edge_x, edge_y
 
-        # Create a list to store the x and y coordinates of the nodes
+    def get_coords_nodes(self) -> tuple:
+        """Get the x and y coordinates of the nodes.
+
+        Returns:
+            tuple: Lists of x and y coordinates of the nodes
+        """
         node_x = []
         node_y = []
         for node in self._model.grid.nodes():
@@ -45,15 +91,40 @@ class TrafficGraph(go.Figure):
             node_x.append(x)
             node_y.append(y)
 
-        node_color = []
-        for node in self._model.grid.nodes(data=True):
-            if node[1]["type"] == "intersection":
-                node_color.append("blue")
-            else:
-                node_color.append("red")
+        return node_x, node_y
 
-        # Create a trace for the nodes
-        node_trace = go.Scatter(
+    def create_trace_edges(self, edge_x: list, edge_y: list) -> go.Scatter:
+        """Create a plotly trace for the edges.
+
+        Args:
+            edge_x (list): X coordinates of the edges
+            edge_y (list): Y coordinates of the edges
+
+        Returns:
+            go.Scatter: Plotly trace for the edges
+        """
+        return go.Scatter(
+            x=edge_x,
+            y=edge_y,
+            line=dict(width=0.5, color="#888"),
+            hoverinfo="none",
+            mode="lines",
+        )
+
+    def create_trace_nodes(
+        self, node_x: list, node_y: list, node_color: list
+    ) -> go.Scatter:
+        """Create a plotly trace for the nodes.
+
+        Args:
+            node_x (list): X coordinates of the nodes
+            node_y (list): Y coordinates of the nodes
+            node_color (list): Colors of the nodes
+
+        Returns:
+            go.Scatter: Plotly trace for the nodes
+        """
+        return go.Scatter(
             x=node_x,
             y=node_y,
             mode="markers",
@@ -65,23 +136,27 @@ class TrafficGraph(go.Figure):
             ),
         )
 
-        # Add the number of connections to the node text
+    def create_node_color(self) -> list:
+        """Create a list of colors for the nodes.
+
+        Returns:
+            list: List of colors for the nodes
+        """
+        return [
+            "blue" if node[1]["type"] == "intersection" else "red"
+            for node in self._model.grid.nodes(data=True)
+        ]
+
+    def create_node_text(self) -> list:
+        """Create a list of text for the nodes.
+
+        Returns:
+            list: List of text for the nodes
+        """
         node_adjacencies = []
         node_text = []
         for node, adjacency_dict in enumerate(self._model.grid.adjacency()):
             node_adjacencies.append(len(adjacency_dict[1]))
             node_text.append("# of connections: " + str(len(adjacency_dict[1])))
 
-        # Update the node trace with the node text and color
-        node_trace.text = node_text
-
-        # Create the figure
-        self.add_traces([edge_trace, node_trace])
-        self.update_layout(
-            title=dict(text="<br>Traffic Grid", font=dict(size=16)),
-            showlegend=False,
-            hovermode="closest",
-            margin=dict(b=20, l=5, r=5, t=40),
-            xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-            yaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-        )
+        return node_text
