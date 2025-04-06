b602aaa485bedd28faf375066dd51c73ad0d4e1f
Jonas Werner <werner-jonas@outlook.com>
2025-04-06 20:07:42 +0200

    add sim script

---
+++ scripts/sim_configs.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+NUM_INTERSECTIONS=("25" "50" "100")
+NUM_BORDERS=("25" "50" "100")
+NUM_CARS=("25" "50" "100")
+MIN_DISTANCE="2"
+MAX_DISTANCE="8"
+STEPS="1000"
+OPTIMIZATION_TYPE="advanced_ml"
+
+for intersections in "${NUM_INTERSECTIONS[@]}"; do
+    for borders in "${NUM_BORDERS[@]}"; do
+        for cars in "${NUM_CARS[@]}"; do
+            python sim.py --num_intersections "$intersections" --num_borders "$borders" --num_cars "$cars" --min_distance "$MIN_DISTANCE" --max_distance "$MAX_DISTANCE" --steps "$STEPS" --optimization_type "$OPTIMIZATION_TYPE"
+        done
+    done
+done
+
+MIN_DISTANCE="9"
+MAX_DISTANCE="15"
+
+for intersections in "${NUM_INTERSECTIONS[@]}"; do
+    for borders in "${NUM_BORDERS[@]}"; do
+        for cars in "${NUM_CARS[@]}"; do
+            python sim.py --num_intersections "$intersections" --num_borders "$borders" --num_cars "$cars" --min_distance "$MIN_DISTANCE" --max_distance "$MAX_DISTANCE" --steps "$STEPS" --optimization_type "$OPTIMIZATION_TYPE"
+        done
+    done
+done
+
+MIN_DISTANCE="16"
+MAX_DISTANCE="22"
+
+for intersections in "${NUM_INTERSECTIONS[@]}"; do
+    for borders in "${NUM_BORDERS[@]}"; do
+        for cars in "${NUM_CARS[@]}"; do
+            python sim.py --num_intersections "$intersections" --num_borders "$borders" --num_cars "$cars" --min_distance "$MIN_DISTANCE" --max_distance "$MAX_DISTANCE" --steps "$STEPS" --optimization_type "$OPTIMIZATION_TYPE"
+        done
+    done
+done
